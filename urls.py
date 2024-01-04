dea948cb6957ecafa306ee44ba5c4595ca7e1d5d
aiswarya038 <139538509+aiswarya038@users.noreply.github.com>
2024-01-04 13:58:09 +0000

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from stud import views
+from managestudent import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -10,9 +10,10 @@ urlpatterns = [
     path('approve/<int:sid>',views.one,name="one"),
     path('approve',views.approve,name="approve"),
     path('delete/<int:did>',views.delstudent,name="delstudent"),
+    path('edit/<int:eid>',views.update_student,name="update_student"),
     path('studentprofile',views.studentprofile,name="studentprofile"),
-
-
+    path('studviewteacher',views.studviewteacher,name="studviewteacher"),
+    path('editstudprofile/<int:eid>',views.edit_studprofile,name="edit_studprofile"),
 
     path('teachreg',views.teachreg,name="teachreg"),
     path('teachview',views.teachview,name="teachview"),
@@ -20,6 +21,10 @@ urlpatterns = [
     path('del/<int:did>',views.delteacher,name="delteacher"),
     path('update/<int:eid>',views.update_teacher,name="update_teacher"),
     path('teachviewstudent',views.teachviewstudent,name="teachviewstudent"),
+    path('editteachprofile/<int:eid>',views.edit_teachprofile,name="edit_teachprofile"),
+
+    path('log',views.logout,name="logout"),
+    path('',views.index,name="index"),
 
 
 
--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth import authenticate,login
-from stud.models import student,teacher
+from managestudent.models import student,teacher
 from django.utils.datastructures import MultiValueDictKeyError
+
 
 
 # Create your views here.
@@ -29,9 +30,10 @@ def login1(request):
             for i in teach:
                 request.session['teach_id']=i.id
                 return render(request,"teacherhome.html")
-        return HttpResponse("success")
+        return HttpResponse("invalid username or password")
     else:
         return render(request,'login.html')
+    # return HttpResponse("success")
         
         
 def studreg(request):
@@ -62,11 +64,36 @@ def delstudent(request,did):
     a.delete()
     return redirect(studview)
 
+def update_student(request,eid):
+    if request.method=='POST':
+        f=request.POST['fn']
+        l=request.POST['ln']
+        a=request.POST['ag']
+        e=request.POST['em']
+        ad=request.POST['add']
+        ph=request.POST['ph']
+        pl=request.POST['pl']
+        u=request.POST['user']
+        p=request.POST['pass']
+        try:
+            g=request.POST['gender']
+        except MultiValueDictKeyError:
+            g='female'
+        c=request.POST['co']
+        x=teacher.objects.filter(id=eid).update(FirstName=f,LastName=l,age=a,email=e,phone=ph,place=pl,address=ad,username=u,password=p,gender=g,course=c)
+        return redirect(studview)
+    else:
+        a=student.objects.get(id=eid)
+        return render(request,"edit_student.html",{"a1":a})
 
 def studentprofile(request):
-    a= request.session['stud_id']
-    profile=teacher.objects.filter(id=a)
-    return render(request,"studentprofile.html",{"a1":profile})
+        a= request.session['stud_id']
+        profile=student.objects.filter(id=a)
+        return render(request,"studentprofile.html",{"a1":profile})
+
+def studviewteacher(request):
+    a=teacher.objects.all()
+    return render(request,"studviewteacher.html",{"a1":a})
 
 def approve(request):
     a=student.objects.filter(value=0)
@@ -74,6 +101,29 @@ def approve(request):
 def one(request,sid):
     a=student.objects.filter(id=sid).update(value=1)
     return redirect(approve)
+
+def edit_studprofile(request,eid):
+    if request.method=='POST':
+        f=request.POST['fn']
+        l=request.POST['ln']
+        a=request.POST['ag']
+        e=request.POST['em']
+        ad=request.POST['add']
+        ph=request.POST['ph']
+        pl=request.POST['pl']
+        u=request.POST['user']
+        p=request.POST['pass']
+        try:
+            g=request.POST['gender']
+        except MultiValueDictKeyError:
+            g='female'
+        c=request.POST['co']
+        
+        x=student.objects.filter(id=eid).update(FirstName=f,LastName=l,age=a,email=e,phone=ph,place=pl,address=ad,username=u,password=p,gender=g,course=c)
+        return redirect(studentprofile)
+    else:
+        a=student.objects.get(id=eid)
+        return render(request,"edit_studprofile.html",{"a1":a})
 
 
 
@@ -137,3 +187,31 @@ def teachprofile(request):
     profile=teacher.objects.filter(id=a)
     return render(request,"teacherprofile.html",{"a1":profile})
 
+
+def edit_teachprofile(request,eid):
+    if request.method=='POST':
+        f=request.POST['fn']
+        l=request.POST['ln']
+        a=request.POST['ag']
+        e=request.POST['em']
+        ad=request.POST['add']
+        ph=request.POST['ph']
+        pl=request.POST['pl']
+        u=request.POST['user']
+        p=request.POST['pass']
+        try:
+            g=request.POST['gender']
+        except MultiValueDictKeyError:
+            g='female'
+        d=request.POST['dept']
+        x=teacher.objects.filter(id=eid).update(FirstName=f,LastName=l,age=a,email=e,phone=ph,place=pl,address=ad,username=u,password=p,gender=g,department=d)
+        return redirect(teachprofile)
+    else:
+        a=teacher.objects.get(id=eid)
+        return render(request,"edit_teachprofile.html",{"a1":a})
+    
+def logout(request):
+    return render(request,"login.html")
+    
+def index(request):
+    return render(request,"index.html")
